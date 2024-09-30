986f212e40c701913fb6b6cfe892cbe9b0e62a06
JasperMunene <jaspermunene2022@gmail.com>
2024-09-30 14:30:02 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+ function superbowlWin(array) {
+    const found = array.find((item) => item.result === "W");
+    if(found){
+        return found.year;
+    }else{
+        return undefined;
+    }
+}
+
